cffe34ff798f2fecef5d79eba23041a0d7fa4f4d
zhangmn <Zhang1880!>
2019-12-06 19:05:01 +0800

    删掉多余的导包

--- src/main/java/com/itheima/expetion/HandlerControllerException.java
+++ src/main/java/com/itheima/expetion/HandlerControllerException.java
@@ -1,14 +1,9 @@
 package com.itheima.expetion;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class HandlerControllerException {
