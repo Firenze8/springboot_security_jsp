4d2683351fa1abca0c810d76015f183f1acd0b59
zhangmn <Zhang1880!>
2019-12-06 16:25:38 +0800

    springboot和spring_security和jsp的初步整合

---
+++ src/main/java/com/itheima/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @RequestMapping
+    @ResponseBody
+    public String hello(){
+        return "success";
+    }
+}
